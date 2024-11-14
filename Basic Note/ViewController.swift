64b328442263994efc49064963166530cb41ea72
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-11-14 20:06:53 +0300

    initial commit

---
+++ Basic Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic Note
+//
+//  Created by Aytaç Bulanık on 14.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
